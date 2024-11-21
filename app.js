200c4975c5bd99db7b3ed68ccac2259daf1884b9
Zachary Springer <zdspringer99@gmail.com>
2024-11-21 10:02:40 -0800

    Work from 11/19/24 SDEV 305 Class

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.post("/success", (req, res) => {
+  const data = req.body;
+  console.log(data);
+
+  res.render("confirmation", { details: data });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
